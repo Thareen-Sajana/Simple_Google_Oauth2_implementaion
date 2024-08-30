a1a8a676fa86893769af1de19b3b8480c0b6858d
Thareen-Sajana <thareensajana0@gmail.com>
2024-08-30 22:57:42 +0530

    updates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 const cookieParser = require('cookie-parser');
+
 const app = express();
 
 const port = 3000;
@@ -12,8 +13,17 @@ app.use(bodyParser.json());
 app.use(express.json())
 app.use(cookieParser())
 
+
 app.use('', routes);
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+// initializeKeys().then(() => {
+    
+//     app.listen(port, () => {
+//         console.log(`Server is running on port ${port}`);
+//     });
+// });
--- model/model.js
+++ model/model.js
@@ -1,15 +1,15 @@
 
 const db = require('../config/db')
 
-exports.save_auth_state = async (state, code_challenge, url)=> {
+exports.save_auth_state = async (state, code_challenge, url, code_verifier)=> {
     await db.query(
-        'INSERT INTO auth_state (state, code_challenge, origin_url) VALUES ($1, $2, $3)',
-        [state, code_challenge, url]
+        'INSERT INTO auth_state (state, code_challenge, origin_url, code_verifier) VALUES ($1, $2, $3, $4)',
+        [state, code_challenge, url, code_verifier]
     )
 }
 
-exports.get_code_challenge = (state) => {
-    return db.query('SELECT code_challenge FROM auth_state WHERE state = $1', [state]);
+exports.get_code_verifier = (state) => {
+    return db.query('SELECT code_verifier FROM auth_state WHERE state = $1', [state]);
 }
 
 exports.save_user = (name, email) => {
@@ -24,10 +24,31 @@ exports.save_user_token_details = (user_id, app_refresh_token, google_access_tok
         [user_id, app_refresh_token, google_access_token, google_refresh_token, new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)])   
 }
 
-exports.get_user_id = (refreshToken) => {
-    return db.query('SELECT user_id FROM user_token WHERE app_refresh_token = $1', [refreshToken]);
+exports.get_user_by_refreshToken = (refreshToken) => {
+    return db.query('SELECT * FROM user_token WHERE app_refresh_token = $1', [refreshToken]);
 }
 
 exports.get_user_details = (user_id) => {
     return db.query('SELECT * FROM users WHERE id = $1', [user_id]);
+}
+
+exports.get_users = async (user_id) => {
+
+    await db.query(`SET LOCAL app.user_id = ${user_id}`);
+    return await db.query('SELECT id, username, email FROM users WHERE id = $1', [user_id]);
+}
+
+exports.update_user_token_table = async (newToken, id) => {
+    await db.query(
+        'UPDATE user_token SET google_access_token = $1 WHERE user_id = $2',
+        [newToken, id]
+    );
+}
+
+exports.add_sign_keys = async (privateKey, publicKey, expirationTime) => {
+    await db.query('INSERT INTO signing_key (private_key, public_key, expires_at) VALUES ($1, $2, $3)', [privateKey, publicKey, expirationTime]);
+}
+
+exports.get_sign_keys = async () => {
+    return await db.query('SELECT * FROM signing_key WHERE expires_at > NOW() ORDER BY expires_at DESC LIMIT 1');
 }
